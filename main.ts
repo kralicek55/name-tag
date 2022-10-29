05267dba11792440648dcc77dbea1d31af885f87
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-10-29 15:54:03 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.showString("Vali")
+    basic.showString("a tata")
+    basic.showString("sli do")
+    basic.showString("SKOLY")
+    basic.showString(".")
+})
